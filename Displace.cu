3a9b2b3f1f636496621eab2397e3e59670ae420b
xintong-osu <tongxin829@gmail.com>
2016-01-18 15:22:07 -0500

    better design for the depth-related deformation

--- Displace.cu
+++ Displace.cu
@@ -9,7 +9,7 @@ void Displace::LoadOrig(float4* v, int num)
 {
 	posOrig.assign(v, v + num);// , posOrig.begin());
 	d_vec_posScreenTarget.assign(num, make_float2(0, 0));
-	d_vec_Dist2LensBtm.assign(num, 0);
+	//d_vec_Dist2LensBtm.assign(num, 0);
 }
 
 struct functor_Object2Clip//: public thrust::unary_function<float,float>
@@ -43,29 +43,21 @@ struct functor_Displace
 	float d;
 	template<typename Tuple>
 	__device__ void operator() (Tuple t){//float2 screenPos, float4 clipPos) {
-		//thrust::make_zip_iterator(
-		//	thrust::make_tuple(
-		//	d_vec_posScreen.begin(),
-		//	d_vec_posClip.begin(),
-		//	d_vec_Dist2LensBtm.begin(),
-		//	first,
-		//	d_vec_posScreenTarget.begin()
 		float2 screenPos = thrust::get<0>(t);
 		float4 clipPos = thrust::get<1>(t);
-		float d2btm = thrust::get<2>(t);
-		float angle = 30 * CUDART_PI_F / 180;
+		float sideSize = 1.0;
 		float2 ret = screenPos;
 		if (clipPos.z < d) {
 			float2 dir = screenPos - make_float2(x, y);
 			float disOrig = length(dir);
 			float ratio = 0.5;
-			float rOut = (r + d2btm * 100 * tan(angle)) / ratio; //including the focus and transition region
+			float rOut = (r + (d - clipPos.z) * r * 32 * sideSize) / ratio; //including the focus and transition region
 			if (disOrig < rOut) {
 				float disNew = G(disOrig / rOut, ratio) * rOut;
 				ret = make_float2(x, y) + dir / disOrig * disNew;
 			}
 		}
-		thrust::get<3>(t) = ret;
+		thrust::get<2>(t) = ret;
 	}
 	functor_Displace(int _x, int _y, int _r, float _d) : x(_x), y(_y), r(_r), d(_d){}
 };
@@ -74,8 +66,10 @@ struct functor_ApproachTarget
 {
 	__device__ float2 operator() (float2 screenPos, float2 screenTarget) {
 		float2 dir = screenTarget - screenPos;
-		//float dis = length(dir);
-		return screenPos + dir * 0.1;
+		if (length(dir) < 0.5) 
+			return screenTarget;
+		else
+			return screenPos + dir * 0.1;
 	}
 };
 
@@ -93,16 +87,16 @@ struct functor_Unproject
 		inv_mv(_inv_mv), inv_pj(_inv_pj), w(_w), h(_h){}
 };
 
-struct func_CompDist2LensBtm{
-	float3 c;
-	matrix4x4 mv;
-	__device__ float operator() (float4 pObj){
-		float4 a = mat4mulvec4(&(mv.v[0].x), make_float4(c.x, c.y, c.z, 1.0f));
-		float4 b = mat4mulvec4(&(mv.v[0].x), pObj);
-		return abs(a.z / a.w - b.z / b.w);//projection * modelview * v;
-	}
-	func_CompDist2LensBtm(float3 _c, matrix4x4 _mv) : c(_c), mv(_mv){}
-};
+//struct func_CompDist2LensBtm{
+//	float3 c;
+//	matrix4x4 mv;
+//	__device__ float operator() (float4 pObj){
+//		float4 a = mat4mulvec4(&(mv.v[0].x), make_float4(c.x, c.y, c.z, 1.0f));
+//		float4 b = mat4mulvec4(&(mv.v[0].x), pObj);
+//		return abs(a.z / a.w - b.z / b.w);//projection * modelview * v;
+//	}
+//	func_CompDist2LensBtm(float3 _c, matrix4x4 _mv) : c(_c), mv(_mv){}
+//};
 //thrust::transform(posOrig.begin(), posOrig.end(), d_vec_Dist2LensBtm.begin(), (l->c, modelview));
 
 
@@ -130,23 +124,19 @@ void Displace::Compute(float* modelview, float* projection, int winW, int winH,
 
 		for (int i = 0; i < lenses.size(); i++) {
 			CircleLens* l = (CircleLens*)lenses[i];
-			thrust::transform(posOrig.begin(), posOrig.end(), 
-				d_vec_Dist2LensBtm.begin(), func_CompDist2LensBtm(l->c, mv));
+			//thrust::transform(posOrig.begin(), posOrig.end(), 
+			//	d_vec_Dist2LensBtm.begin(), func_CompDist2LensBtm(l->c, mv));
 			thrust::for_each(
 				thrust::make_zip_iterator(
 				thrust::make_tuple(
 				d_vec_posScreen.begin(), 
 				d_vec_posClip.begin(), 
-				d_vec_Dist2LensBtm.begin(),
-				//first,
 				d_vec_posScreenTarget.begin()
 				)),
 				thrust::make_zip_iterator(
 				thrust::make_tuple(
 				d_vec_posScreen.end(),
 				d_vec_posClip.end(),
-				d_vec_Dist2LensBtm.end(),
-				//first+size,
 				d_vec_posScreenTarget.end()
 				)),
 				functor_Displace(l->x, l->y, l->radius, l->GetClipDepth(modelview, projection)));
